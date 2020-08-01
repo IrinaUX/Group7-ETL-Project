43bca782cd86be32fe313855751661ec142b4623
Irina Kim <irinahpda@gmail.com>
2020-08-01 16:46:51 -0500

    Minor changes.

--- sql_scripts/SQL_final.sql
+++ sql_scripts/SQL_final.sql
@@ -13,7 +13,6 @@ SELECT * FROM combined_gross_info;
 ALTER TABLE combined_gross_info ADD COLUMN international_revenue_usd BIGINT;
 UPDATE combined_gross_info SET international_revenue_usd = (total_revenue_usd - domestic_revenue_usd);
 
-
 -- Replace null values with 0 in combined_gross_info
 UPDATE combined_gross_info SET international_revenue_usd = 0 WHERE international_revenue_usd IS NULL;
 UPDATE combined_gross_info SET domestic_revenue_usd = 0 WHERE domestic_revenue_usd IS NULL;
@@ -41,14 +40,14 @@ ALTER TABLE director ADD COLUMN director_id SERIAL PRIMARY KEY;
 
 -- Add transient tables by merging
 SELECT movie_id, actor_id FROM movies, actor;
+SELECT movie_id FROM movies;
+SELECT actor_id FROM actor;
 
 -- CREATE actor_movie table from movies table
 CREATE TABLE actor_movie AS
 SELECT movie_id FROM movies  
 OUTER JOIN total_gross_info
 ON total_gross_info.name = domestic_gross_info.name_domestic;
-
-
 
 -- Examples of defining the PK and FK
 ALTER TABLE test1 ADD COLUMN id SERIAL PRIMARY KEY;
@@ -57,9 +56,3 @@ ALTER TABLE country ADD FOREIGN KEY (total_revenue_usd.country); -- Add PRIMARY 
 ALTER TABLE table_name DROP CONSTRAINT primary_key_constraint; -- Drop PK 
 ALTER TABLE country ADD FOREIGN KEY (country) REFERENCES total_gross_info(country); -- Add FOREIGN KEY
 
-
--- Remove unnecessary table from schema
-DROP TABLE IF EXISTS combined_gross_info;
-DROP TABLE IF EXISTS total_gross_info;
-DROP TABLE IF EXISTS domestic_gross_info;
-DROP TABLE IF EXISTS fetched_missing;
